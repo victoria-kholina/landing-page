6b57464fa8aab1b362a5e4e4d2c71fdc3dea8dac
victoria-kholina <kholina.victoria@gmail.com>
2017-06-26 22:29:48 +0300

    header and slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,31 +1,16 @@
 jQuery(document).ready(function(){
     
-getDropdown();  
-$(window).resize(getDropdown); 
-    
     var mySwiper = new Swiper ('.swiper-container', {
-      loop: false,
-      pagination: '.swiper-pagination',
-      paginationClickable: true,
-      paginationBulletRender: function (swiper, index, className) {
-            return '<span class="' + className + '">' + (index + 1) + '</span>';
-    }
-    }); 
-
-    function getDropdown(){
-        var winWidth = $(window).width();
-        if(winWidth<='768'){ 
-            $('.main-menu button').removeClass("hidden").addClass("show");
-            $('.main-menu').addClass("dropdown btn-group");
-            $('.main-menu ul').addClass("dropdown-menu");
-        } else { 
-            $('.main-menu button').removeClass("show").addClass("hidden");
-            $('.main-menu').removeClass("dropdown");
-            $('.main-menu ul').removeClass("dropdown-menu");     
-        };
-        };
-    
-    
         
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        spaceBetween: 5
+        
+//  Uncomment for autoplay slider
+//        autoplay: 3000,
+//        autoplayDisableOnInteraction: false
+    }); 
+    
+
 });//end jQuery
                     
--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,29 @@
 var gulp = require('gulp'), 
     sass = require('gulp-sass'),
+    cssbeautify = require('gulp-cssbeautify'),
     browserSync = require('browser-sync');
+
+// HTML //
+
+gulp.task('html', function(){
+  gulp.src('app/index.html')
+  .pipe(browserSync.reload({stream:true}));
+});
+
+// JavaScript //
+
+gulp.task('js', function(){
+  gulp.src('app/js/main.js')
+  .pipe(browserSync.reload({stream:true}));
+});
 
 // SASS //
 
 gulp.task('sass', function(){
     return gulp.src('app/sass/main.sass') 
         .pipe(sass()) 
-        .pipe(gulp.dest('app/css')) 
+        .pipe(cssbeautify())
+        .pipe(gulp.dest('app/css'))
         .pipe(browserSync.reload({stream: true})) // live reload/updating CSS 
 });
 
@@ -22,6 +38,8 @@ gulp.task('browser-sync', function() {
     });
 });
 
-gulp.task('watch', ['browser-sync', 'sass'], function() {
+gulp.task('watch', ['browser-sync', 'html', 'js', 'sass'], function() {
     gulp.watch('app/sass/**/*.sass', ['sass']); // Monitoring sass files
+    gulp.watch('app/index.html', ['html']);
+    gulp.watch('app/js/main.js', ['js']); 
 });
