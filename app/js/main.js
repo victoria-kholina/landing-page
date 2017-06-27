60b151b3fe5bd3c7fc671f67604d6c1e782aa219
victoria-kholina <kholina.victoria@gmail.com>
2017-06-27 21:18:59 +0300

    murkup without media

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,13 @@
 jQuery(document).ready(function(){
     
     var mySwiper = new Swiper ('.swiper-container', {
-        
+        loop: false,
+      pagination: '.swiper-pagination',
+      paginationClickable: true,
+    
+      paginationBulletRender: function (swiper, index, className) {
+            return '<span class="' + className + '">' + (index + 1) + '</span>';
+    },
         nextButton: '.swiper-button-next',
         prevButton: '.swiper-button-prev',
         spaceBetween: 5
@@ -11,6 +17,22 @@ jQuery(document).ready(function(){
 //        autoplayDisableOnInteraction: false
     }); 
     
+    
+    $('.buy-now-btn').hover(
+        function() {
+            $(this).parents("table").find("thead tr:first-child").css("background","#44c0c2");
+        }, 
+        function() {
+            $(this).parents("table").find("thead tr:first-child").css("background","#485460");
+        }
+     );
+    
+    $(".main-menu li a").click(function () {
+        var elemClick = $(this).attr("href");  
+        var elemPos = $(elemClick).offset().top;
+        $("body,html").animate({scrollTop: elemPos }, 800);
+        return false;
+      });
 
 });//end jQuery
                     
