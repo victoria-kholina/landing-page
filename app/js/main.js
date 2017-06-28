e8ef435df10021b6e0e27e235ae61222e9356bde
victoria-kholina <kholina.victoria@gmail.com>
2017-06-28 18:48:22 +0300

    form validation is done

--- app/js/main.js
+++ app/js/main.js
@@ -56,6 +56,89 @@ jQuery(document).ready(function(){
             }
         };
     };
+    
+        //FORM VALIDATION
+    
+    function showErrorMessage(input, textErr){
+        var errMessage = input.parents("form").find(".error-message");
+            errMessage.text(textErr).css({"margin-bottom": "10px", "font-size": "0.8em", "text-align": "left"});
+            errMessage.animate({'paddingLeft':'10px'},400).animate({'paddingLeft':'5px'},400); 
+    }
+	
+    function hideErrorMessage(inpt){
+        if(inpt.hasClass("error")){
+            inpt.removeClass('error'); 
+            inpt.parents("form").find(".error-message").empty();
+        }	
+    }
+    
+    $('.footer-form').unbind().blur( function(){
+        var name = $(this).attr('name');
+        var value = $(this).val();
+              
+        switch(name) {
+             case 'user-name': 
+                var regExpName = /^[a-zA-Zа-яА-Я]+$/;
+                
+                        if(value=="" || regExpName.test(value)) { //empty input we will check on form submit
+                               $(this).addClass('not-error');
+                                hideErrorMessage($(this)); 
+                        } else {
+                                $(this).removeClass('not-error').addClass('error');
+                                 showErrorMessage($(this), "*Please, enter your name including only letters");
+                         }
+            break;
+            
+            case 'user-email': 
+                var regExpEmail = /^([a-z0-9_.-]+)@([a-z0-9_.-])+\.[a-z0-9]+\.*[a-z]*/;
+                
+                        if(value=="" || regExpEmail.test(value)) {
+		                      $(this).addClass('not-error');
+                                hideErrorMessage($(this));
+                        } else {
+                                $(this).removeClass('not-error').addClass('error');
+                                showErrorMessage($(this), "*Email is not valid. Please enter a valid email");
+                        }
+            break;
+            
+            case 'user-message':
+                
+                        if(value == "") {
+                                $(this).removeClass('not-error').addClass('error');
+                                showErrorMessage($(this), "*Not all fields are filled");
+                        } else {
+                             $(this).addClass('not-error');
+                            hideErrorMessage($(this));
+                        }
+            break;
+                          
+        };//end switch
+    });// end blur
+    
+    
+    //FORM SUBMIT 
+      
+    $('#form1').submit(function () { 
+        var entryField = $(this).find(".footer-form");
+        
+        entryField.each(function () {
+            if ($(this).val() == "") {
+                $(this).removeClass('not-error').addClass('error');
+                $(this).trigger("focus");
+                showErrorMessage($(this), "*Not all fields are filled");
+            }
+        });
+        
+        if(entryField.hasClass("error")){ 
+                $(".error").trigger("focus");
+                showErrorMessage($(this), "*Not all fields are filled");
+            } else {
+                alert("OK");
+            }
+        
+    return false;
+        
+    }); //end submit
 
 });//end jQuery
                     
